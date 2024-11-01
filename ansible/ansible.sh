5da6b395c2d820b0146cda566b92693d5bd5ae99
Hichame Aniter <animer.hichame@gmail.com>
2024-11-02 00:51:15 +0100

    Install Containerd

--- ansible/ansible.sh
+++ ansible/ansible.sh
@@ -3,6 +3,6 @@
 # Global Variables
 ANSIBLE_DIR=$(dirname "$(realpath "$0")")
 
-# Init VMs && Install Docker
+# Init VMs && Install Containerd
 
 ansible-playbook -i $ANSIBLE_DIR/my_inventory/hosts.aws_ec2.yaml $ANSIBLE_DIR/playbook.yaml --private-key $ANSIBLE_DIR/myKey.pem -u ec2-user
